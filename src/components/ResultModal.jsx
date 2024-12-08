5f1fc59659b129271cde75537bb7d223ed72c334
Martialdepaul <bourangoneulomimartialdepaul@gmail.com>
2024-12-08 23:36:13 +0100

    Initial commit

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,45 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const fromattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(1 - (remainingTime / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{fromattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+ResultModal.propTypes = {
+  targetTime: PropTypes.number.isRequired,
+  remainingTime: PropTypes.number.isRequired,
+  onReset: PropTypes.func.isRequired,
+};
+export default ResultModal;
